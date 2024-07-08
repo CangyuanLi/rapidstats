bf52a7707fd5a100b8f7cc0bfdb809c516e519e0
Cangyuan Li <everest229@gmail.com>
2024-07-08 00:30:14 -0400

    update tests

--- tests/test_distributions.py
+++ tests/test_distributions.py
@@ -3,10 +3,20 @@ import scipy.stats
 
 import rapidstats
 
+Q = [i / 100 for i in range(100)]
 
-@pytest.mark.parametrize("q", [i / 100 for i in range(100)])
+
+@pytest.mark.parametrize("q", Q)
 def test_norm_ppf(q: float):
     ref = scipy.stats.norm.ppf(q)
     rs = rapidstats.norm.ppf(q)
+
+    pytest.approx(ref) == rs
+
+
+@pytest.mark.parametrize("q", Q)
+def test_norm_cdf(q: float):
+    ref = scipy.stats.norm.cdf(q)
+    rs = rapidstats.norm.cdf(q)
 
     pytest.approx(ref) == rs
