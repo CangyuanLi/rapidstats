b360a148d5b4c9ead24b96428e9dd4ee43405491
Cangyuan Li <everest229@gmail.com>
2024-07-21 01:10:48 -0400

    add docs for distributions

--- python/rapidstats/_distributions.py
+++ python/rapidstats/_distributions.py
@@ -2,10 +2,40 @@ from ._rustystats import _norm_cdf, _norm_ppf
 
 
 class norm:
+    """Functions for working with a normal continuous random variable."""
+
     @staticmethod
     def ppf(q: float) -> float:
+        r"""The percent point function. Also called the quantile, percentile, inverse
+        CDF, or inverse distribution function. Computes the value of a random variable
+        such that its probability is \( \leq q \). If `q` is 0, it returns negative
+        infinity, if `q` is 1, it returns infinity. Any number outside of [0, 1] will
+        result in NaN.
+
+        Parameters
+        ----------
+        q : float
+            Probability value
+
+        Returns
+        -------
+        float
+            Likelihood a random variable is realized in the range at or below `q` for
+            the normal distribution.
+        """
         return _norm_ppf(q)
 
     @staticmethod
-    def cdf(q: float) -> float:
-        return _norm_cdf(q)
+    def cdf(x: float) -> float:
+        """The cumulative distribution function.
+
+        Parameters
+        ----------
+        x : float
+
+        Returns
+        -------
+        float
+            The probability a random variable will take a value \( \leq x \)
+        """
+        return _norm_cdf(x)
