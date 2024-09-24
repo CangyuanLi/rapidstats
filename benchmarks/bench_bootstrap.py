1acc11206ba043f5b73e7edd4921b072b89cd9fd
Cangyuan Li <everest229@gmail.com>
2024-09-23 20:35:25 -0700

    benchmark bootstrap

---
+++ benchmarks/bench_bootstrap.py
@@ -0,0 +1,13 @@
+import concurrent.futures
+
+import rapidstats
+
+BS = rapidstats.Bootstrap()
+
+
+def bench_bootstrap_roc_auc():
+    pass
+
+
+# def bench_python_bootstrap_roc_auc():
+#     with concu
