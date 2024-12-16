d77c3dd85a71410ae3f05e4d8b43cf30047751a0
Cangyuan Li <everest229@gmail.com>
2024-12-15 16:41:16 -0800

    fix: remove deleted functions

--- python/rapidstats/__init__.py
+++ python/rapidstats/__init__.py
@@ -3,8 +3,6 @@ from ._corr import correlation_matrix
 from ._distributions import norm
 from ._metrics import (
     adverse_impact_ratio,
-    appr_rate_at_thresholds,
-    bad_rate_at_thresholds,
     brier_loss,
     confusion_matrix,
     confusion_matrix_at_thresholds,
