64d78d0df3a254f74797294cf835d815370b83a0
Cangyuan Li <everest229@gmail.com>
2025-03-19 14:44:59 -0700

    tests: add test for saving and loading min max scaler

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,3 +1,5 @@
+import tempfile
+
 import numpy as np
 import polars as pl
 import polars.selectors as cs
@@ -10,42 +12,60 @@ SEED = 208
 
 np.random.seed(SEED)
 
+n_rows = 1_000
+cols = ["A", "B"]
+DATA = (
+    pl.DataFrame({c: np.random.rand(n_rows) for c in cols})
+    .with_columns(
+        pl.int_range(n_rows)
+        .sample(pl.len(), with_replacement=True, seed=SEED + i)
+        .truediv(n_rows)
+        .alias(f"prob_{c}")
+        for i, c in enumerate(cols)
+    )
+    .with_columns(
+        pl.when(pl.col(f"prob_{c}").gt(0.9))
+        .then(None)
+        .when(pl.col(f"prob_{c}").lt(0.1))
+        .then(float("nan"))
+        .otherwise(pl.col(c))
+        .alias(c)
+        for c in cols
+    )
+    .drop(cs.starts_with("prob_"))
+)
+
 
 def test_min_max_scaler():
-    n_rows = 1_000
-    cols = ["A", "B"]
-    data = (
-        pl.DataFrame({c: np.random.rand(n_rows) for c in cols})
-        .with_columns(
-            pl.int_range(n_rows)
-            .sample(pl.len(), with_replacement=True, seed=SEED + i)
-            .truediv(n_rows)
-            .alias(f"prob_{c}")
-            for i, c in enumerate(cols)
-        )
-        .with_columns(
-            pl.when(pl.col(f"prob_{c}").gt(0.9))
-            .then(None)
-            .when(pl.col(f"prob_{c}").lt(0.1))
-            .then(float("nan"))
-            .otherwise(pl.col(c))
-            .alias(c)
-            for c in cols
-        )
-        .drop(cs.starts_with("prob_"))
-    )
 
     np.testing.assert_allclose(
-        sklearn.preprocessing.MinMaxScaler().fit_transform(data),
-        rapidstats.preprocessing.MinMaxScaler().fit_transform(data).to_numpy(),
+        sklearn.preprocessing.MinMaxScaler().fit_transform(DATA),
+        rapidstats.preprocessing.MinMaxScaler().fit_transform(DATA).to_numpy(),
     )
 
     # test inverse transform
     scaler = rapidstats.preprocessing.MinMaxScaler()
 
     polars.testing.assert_frame_equal(
-        data, scaler.inverse_transform(scaler.fit_transform(data))
+        DATA, scaler.inverse_transform(scaler.fit_transform(DATA))
     )
+
+
+def test_min_max_scaler_save():
+    with tempfile.TemporaryFile() as f:
+        scaler = rapidstats.preprocessing.MinMaxScaler()
+        scaler.fit(DATA)
+        scaler.save(f)
+
+        scaler_loaded = rapidstats.preprocessing.MinMaxScaler().load(f)
+
+        polars.testing.assert_frame_equal(
+            scaler.min_.to_polars(), scaler_loaded.min_.to_polars()
+        )
+        polars.testing.assert_frame_equal(
+            scaler.scale_.to_polars(), scaler_loaded.scale_.to_polars()
+        )
+        assert scaler.feature_names_in_ == scaler_loaded.feature_names_in_
 
 
 def test_one_hot_encoder():
